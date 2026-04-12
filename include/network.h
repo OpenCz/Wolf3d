8402d36eaa30592de7fda408a545b554d3d8686a
czp <celenzo.peuch@epitech.eu>
2026-04-12 23:04:30 +0200

    feat: tmp struct game

--- include/network.h
+++ include/network.h
@@ -7,6 +7,7 @@
 
 #include <SFML/Network.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #ifndef NET_H
     #define NET_H
@@ -16,6 +17,29 @@
 
     #define MAX_PLAYERS 4
     #define TICK_RATE 20
+
+typedef struct s_player_state {
+    int id;
+    float x;
+    float y;
+    float angle;
+    int health;
+} t_player_state;
+
+typedef struct s_input_msg {
+    int player_id;
+    bool forward;
+    bool backward;
+    bool turn_left;
+    bool turn_right;
+    bool shoot;
+} t_input_msg;
+
+typedef struct s_world_state {
+    t_player_state players[MAX_PLAYERS];
+    int player_count;
+    unsigned int tick;
+} t_world_state;
 
 typedef enum {
     MSG_PLAYER_JOIN,
