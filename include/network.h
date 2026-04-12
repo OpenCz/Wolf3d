fb1ea3a21f8a1fef8ab2eaeb727c1441625b8b3f
czp <celenzo.peuch@epitech.eu>
2026-04-12 23:01:15 +0200

    feat: enum msg create

--- include/network.h
+++ include/network.h
@@ -11,5 +11,20 @@
 #ifndef NET_H
     #define NET_H
 
+    #define PORT_TCP 4999
+    #define PORT_UDP 5000
+
+    #define MAX_PLAYERS 4
+    #define TICK_RATE 20
+
+typedef enum {
+    MSG_PLAYER_JOIN,
+    MDG_PLAYER_QUIT,
+    MSG_PLAYER_MOVE,
+    MSG_PLAYER_SHOOT,
+    MSG_PLAYER_DEATH,
+    MSG_PLAYER_INPUT,
+    MSG_WOLRD_STATE
+} net_status_t;
 
 #endif
